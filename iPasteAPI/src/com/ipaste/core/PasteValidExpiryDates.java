4b1e0ace5154fc7db37bdf3caf2e052d19d3bfa0
iPasteEu <support@ipaste.eu>
2013-06-24 19:46:43 +0200

    ...

---
+++ iPasteAPI/src/com/ipaste/core/PasteValidExpiryDates.java
@@ -0,0 +1,25 @@
+package com.ipaste.core;
+
+public interface PasteValidExpiryDates {
+    /* Minutes */
+    public final static String ONE_MINUTE = "1 MINUTE";
+    public final static String FIVE_MINUTES = "5 MINUTE";
+    public final static String TEN_MINUTES = "10 MINUTE";
+    public final static String THIRTY_MINUTES = "30 MINUTE";
+    /* Hours */
+    public final static String ONE_HOUR = "1 HOUR";
+    public final static String SIX_HOURS = "6 HOUR";
+    public final static String TWELVE_HOURS = "12 HOUR";
+    /* Days */
+    public final static String ONE_DAY = "1 DAY";
+    public final static String SEVEN_DAYS = "7 DAY";
+    public final static String FOURTEEN_DAYS = "14 DAY";
+    /* Months */
+    public final static String ONE_MONTH = "1 MONTH";
+    /* Years */
+    public final static String ONE_YEAR = "1 YEAR";
+    public final static String FIVE_YEARS = "5 YEAR";
+    public final static String TEN_YEARS = "10 YEAR";
+    public final static String ONE_HUNDRED_YEARS = "100 YEAR";
+    public final static String ONE_THOUSAND_YEARS = "1000 YEAR";
+}
