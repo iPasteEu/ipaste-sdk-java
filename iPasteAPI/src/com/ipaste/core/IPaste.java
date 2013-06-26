e8d69711f84b4c05774ac2b9994549a161e0ec7a
iPasteEu <support@ipaste.eu>
2013-06-26 13:55:29 +0200

    bug fixing

--- iPasteAPI/src/com/ipaste/core/IPaste.java
+++ iPasteAPI/src/com/ipaste/core/IPaste.java
@@ -1,10 +1,14 @@
 package com.ipaste.core;
 
 import java.io.BufferedReader;
-import java.io.DataOutputStream;
+import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
@@ -15,9 +19,7 @@ import java.util.Iterator;
 import java.util.List;
 
 import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
+import org.json.simple.JSONValue;
 
 import com.ipaste.exception.IPasteException;
 import com.ipaste.paste.Paste;
@@ -29,59 +31,115 @@ import com.ipaste.response.IPasteExtraResponseFormat;
 import com.ipaste.response.IPasteResponseFormat;
 
 public class IPaste implements IPasteCore {
+	/**
+	 * initialize this variable if you want to use only the public IPaste(String
+	 * username, String password) constructor
+	 */
+	private final static String DEV_KEY = null;
+
 	private String devKey;
 	private String username;
 	private String password;
 	private String tmpKey;
 
-	public IPaste(){
-		
+	/**
+	 * it's not recommended to use this constructor because it does not validate
+	 * and initialize the username and password class instance variables (you
+	 * have to set them manually).
+	 * 
+	 * @throws IPasteException
+	 */
+	public IPaste() throws IPasteException {
+		/**
+		 * if DEV_KEY was initialized, you can use the public IPaste(String
+		 * username, String password) constructor, otherwise you should use the
+		 * public IPaste(String devKey, String username, String password)
+		 * constructor
+		 */
+		if (DEV_KEY != null) {
+			this.validateDeveloperKey(DEV_KEY);
+			this.devKey = DEV_KEY;
+		}
+
 	}
-	
-	private IPaste(String username, String password) throws IPasteException {
+
+	/**
+	 * use this constructor if you defined the DEV_KEY variable. This
+	 * constructor initialize the username and password variables just after it
+	 * called the parameter free constructor. Finally it calls the login()
+	 * method.
+	 * 
+	 * @param username
+	 * @param password
+	 * @throws IPasteException
+	 */
+	public IPaste(String username, String password) throws IPasteException {
+		this();
 		this.validateUsername(username);
 		this.validateRawPassword(password);
-
 		this.username = this.md5(username.toUpperCase());
 		this.password = this.md5(password);
-
+		this.login();
 	}
 
+	/**
+	 * initializes class instance variables and tries to login. It ignores the
+	 * DEV_KEY variable. It tries to make a login.
+	 * 
+	 * @param devKey
+	 * @param username
+	 * @param password
+	 * @throws IPasteException
+	 */
 	public IPaste(String devKey, String username, String password) throws IPasteException {
-		this(username, password);
-		this.validateTmpKey(tmpKey);
+		this.validateUsername(username);
+		this.validateRawPassword(password);
+		this.username = this.md5(username.toUpperCase());
+		this.password = this.md5(password);
+		this.validateDeveloperKey(devKey);
 		this.devKey = devKey;
 		// tries to login
 		this.login();
 	}
 
+	/**
+	 * validates login data and tries to login
+	 * @return String developer key
+	 * @throws IPasteException
+	 */
 	@Override
 	public String login() throws IPasteException {
 		this.validateHashedPassword(this.password);
-		this.validateTmpKey(this.tmpKey);
 		this.validateUsername(this.username);
+		this.validateDeveloperKey(this.devKey);
 
 		String response;
 		try {
-			response = this.call("act=login&a=" + URLEncoder.encode(this.tmpKey, "UTF-8") + URLEncoder.encode(this.username, "UTF-8") + URLEncoder.encode(this.password, "UTF-8"));
+			response = this.call("act=login&a=" + URLEncoder.encode(this.devKey, "UTF-8") + URLEncoder.encode(this.username, "UTF-8") + URLEncoder.encode(this.password, "UTF-8")).trim();
 		} catch (UnsupportedEncodingException e) {
 			throw new IPasteException(CLIENT_EXCEPTION + "internal error");
 		}
 		if (this.isErrorResponse(response))
 			throw new IPasteException(response);
+		// check if the returned tmpKey is correct
+		this.validateTmpKey(response);
 		this.tmpKey = response;
 		return response;
 	}
 
+	/**
+	 * tries to login using the login data passed as parameter
+	 * @param devKey
+	 * @param username
+	 * @param password
+	 * @return String developer key
+	 * @throws IPasteException
+	 */
 	@Override
 	public String login(String devKey, String username, String password) throws IPasteException {
-		this.validateEmpty(tmpKey);
-		this.validateEmpty(username);
-		this.validateEmpty(password);
-
-		this.devKey = devKey;
 		this.username = this.md5(username.toUpperCase());
 		this.password = this.md5(password);
+		this.devKey = devKey;
 		return this.login();
 	}
 
@@ -102,9 +160,12 @@ public class IPaste implements IPasteCore {
 		this.validateTmpKey(this.tmpKey);
 		this.validateField(responseFormat, IPasteResponseFormat.class);
 		String response = this.call("act=get_all_user_pastes&frm=" + responseFormat + "&a=" + this.tmpKey);
-		if (!this.isErrorResponse(response))
-			list = this.jsonToIntegerList(response);
-		else
+		if (!this.isErrorResponse(response)) {
+			if (responseFormat == IPasteResponseFormat.JSON)
+				list = this.jsonToIntegerList(response);
+			else if (responseFormat == IPasteResponseFormat.RAW || responseFormat == IPasteResponseFormat.TEXT)
+				list = this.textToIntegerList(response);
+		} else
 			throw new IPasteException(response);
 		return list;
 	}
@@ -301,30 +362,36 @@ public class IPaste implements IPasteCore {
 			connection.setUseCaches(false);
 			connection.setDoInput(true);
 			connection.setDoOutput(true);
-			try (DataOutputStream wr = new DataOutputStream(connection.getOutputStream());) {
-				wr.writeBytes(param);
-				wr.flush();
-			}
+
+			OutputStream os = connection.getOutputStream();
+			BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
+			writer.write(param);
+			writer.close();
+			os.close();
 			InputStream is = connection.getInputStream();
+
 			StringBuilder response = null;
 			BufferedReader br = null;
 			try {
 				br = new BufferedReader(new InputStreamReader(is));
-				String line;
+				String linee;
 				response = new StringBuilder();
-				while ((line = br.readLine()) != null) {
-					response.append(line);
+
+				while ((linee = br.readLine()) != null) {
+					response.append(linee + "\r\n");
 				}
-			} catch (Exception e) {
+				br.close();
+			} catch (IOException e) {
 				throw new IPasteException(CLIENT_EXCEPTION + e);
 			} finally {
 				if (br != null)
 					try {
 						br.close();
-					} catch (Exception e2) {
+					} catch (IOException e2) {
 						throw new IPasteException(CLIENT_EXCEPTION + e2);
 					}
 			}
+			connection.disconnect();
 			return response.toString();
 
 		} catch (Exception e) {
@@ -336,12 +403,19 @@ public class IPaste implements IPasteCore {
 		}
 	}
 
-	private String md5(String str) throws IPasteException {
+	public String md5(String input) throws IPasteException {
 		try {
-			MessageDigest md5 = MessageDigest.getInstance("MD5");
-			return md5.digest(str.getBytes()).toString();
+			MessageDigest md = MessageDigest.getInstance("MD5");
+			byte[] messageDigest = md.digest(input.getBytes());
+			BigInteger number = new BigInteger(1, messageDigest);
+			String hashtext = number.toString(16);
+			// Now we need to zero pad it if you actually want the full 32
+			// chars.
+			while (hashtext.length() < 32) {
+				hashtext = "0" + hashtext;
+			}
+			return hashtext;
 		} catch (NoSuchAlgorithmException e) {
-			e.printStackTrace();
 			throw new IPasteException(e);
 		}
 	}
@@ -350,6 +424,7 @@ public class IPaste implements IPasteCore {
 		return (str == null || str.isEmpty());
 	}
 
+	@SuppressWarnings("unused")
 	private void validateEmpty(String str) throws IPasteException {
 		if (this.isEmpty(str))
 			throw new IPasteException(CLIENT_EXCEPTION + "invalid input value");
@@ -366,41 +441,54 @@ public class IPaste implements IPasteCore {
 	}
 
 	private void validateHashedPassword(String hashedPassword) throws IPasteException {
-		if (this.isEmpty(password) || tmpKey.length() != 32)
-			throw new IPasteException(CLIENT_EXCEPTION + "invalid password: " + password);
+		if (this.isEmpty(hashedPassword) || hashedPassword.length() != 32)
+			throw new IPasteException(CLIENT_EXCEPTION + "invalid hashed password: " + hashedPassword);
+	}
+
+	private void validateDeveloperKey(String developerKey) throws IPasteException {
+		if (this.isEmpty(developerKey) || developerKey.length() != 32)
+			throw new IPasteException(CLIENT_EXCEPTION + "invalid developer key: " + developerKey);
 	}
 
 	private void validateRawPassword(String rawPassword) throws IPasteException {
-		if (this.isEmpty(password) || tmpKey.length() < 6 || tmpKey.length() > 32)
-			throw new IPasteException(CLIENT_EXCEPTION + "invalid password: " + password);
+		if (this.isEmpty(rawPassword) || rawPassword.length() < 6 || rawPassword.length() > 32)
+			throw new IPasteException(CLIENT_EXCEPTION + "invalid password: " + rawPassword);
 	}
 
 	private void validateField(String field, Class<?> cl) throws IPasteException {
 		try {
-			cl.getField(field);
+			cl.getField(field.toUpperCase());
 		} catch (NoSuchFieldException | SecurityException e) {
-			throw new IPasteException(CLIENT_EXCEPTION + "invalid input");
+			throw new IPasteException(CLIENT_EXCEPTION + "invalid input: " + e);
 		}
 	}
 
 	private List<Integer> jsonToIntegerList(String response) throws IPasteException {
 		List<Integer> list = null;
-		JSONParser parser = new JSONParser();
+		Object obj = JSONValue.parse(response);
+		list = new ArrayList<Integer>();
+		// loop array
+		JSONArray msg = (JSONArray) obj;
+		@SuppressWarnings("unchecked")
+		// Using legacy API
+		Iterator<String> iterator = msg.iterator();
+		while (iterator.hasNext()) {
+			list.add(Integer.parseInt(iterator.next()));
+		}
+		return list;
+	}
+
+	private List<Integer> textToIntegerList(String response) throws IPasteException {
+		List<Integer> list = new ArrayList<Integer>();
+		String[] arr = response.split("\\r?\\n");
 		try {
-			Object obj = parser.parse(response);
-			JSONObject jsonObject = (JSONObject) obj;
-			list = new ArrayList<Integer>();
-			// loop array
-			JSONArray msg = (JSONArray) jsonObject.keySet();
-			@SuppressWarnings("unchecked")
-			// Using legacy API
-			Iterator<String> iterator = msg.iterator();
-			while (iterator.hasNext()) {
-				list.add(Integer.parseInt(iterator.next()));
+			for (String s : arr) {
+				list.add(Integer.parseInt(s));
 			}
-		} catch (ParseException e) {
+		} catch (NumberFormatException e) {
 			throw new IPasteException(CLIENT_EXCEPTION + e);
 		}
+
 		return list;
 	}
 
--- iPasteAPI/src/com/ipaste/sample/TryMe.java
+++ iPasteAPI/src/com/ipaste/sample/TryMe.java
@@ -1,15 +1,28 @@
 package com.ipaste.sample;
 
 import com.ipaste.core.IPaste;
+import com.ipaste.exception.IPasteException;
+import com.ipaste.response.IPasteResponseFormat;
 
 public class TryMe {
+
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 
-		IPaste ipaste = new IPaste();
+		try {
+			IPaste ipaste = new IPaste(DEV_KEY, USERNAME, PASSWORD);
+			System.out.println("login() - Your tmpKey is: "+ipaste.getTmpKey());
+			System.out.println("login(dev_key, username, password) - Your tmpKey is: "+ipaste.login(DEV_KEY, USERNAME, PASSWORD));
+			
+			System.out.println("getUserPastes() - Your pastes are: "+ipaste.getUserPastes());
+			System.out.println("getUserPastes(IPasteResponseFormat.TEXT) - Your pastes are: "+ipaste.getUserPastes(IPasteResponseFormat.TEXT));
+			
+		} catch (IPasteException e) {
+			System.out.println(e);
+		}
 
 	}
 
